a221c16d88a03cd7e707a704b99676c578d870c6
Meine-Wetterstation <humstuben@yahoo.com>
2026-03-15 21:30:29 +0100

    Update updateWeather.js

--- updateWeather.js
+++ updateWeather.js
@@ -22,12 +22,30 @@ const API_KEY = "27602f1bbb8e3dd3587a1da6e3de24b6";
 const LAT = 47.43602311386345;
 const LON = 16.25550536923543;
 async function updateWeather() {
-  console.log("TEST START");
+  console.log("Hole OpenWeather...");
 
-  await update(ref(db, "weather/openweather/raw"), {
-    test: "OK",
-    weather: [{ icon: "01d" }]
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${API_KEY}&units=metric`;
+  const ow = await fetch(url).then(r => r.json());
+
+  console.log("OpenWeather OK:", ow.weather[0].description);
+
+  // LIVE-DATEN
+  await update(ref(db, "weather/live"), {
+    temp: ow.main.temp,
+    humidity: ow.main.humidity,
+    pressure: ow.main.pressure,
+    icon: ow.weather[0].icon,
+    timestamp: Math.floor(Date.now() / 1000)
   });
 
-  console.log("TEST ENDE");
+  // ROHDATEN
+  await update(ref(db, "weather/openweather/raw"), ow);
+
+  // SONNE
+  await update(ref(db, "weather/sun"), {
+    sunrise: ow.sys.sunrise,
+    sunset: ow.sys.sunset
+  });
+
+  console.log("Firebase aktualisiert!");
 }
